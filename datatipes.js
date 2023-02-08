be3ed7b209bb370769e2a518aa9a03bcb2da4f54
Andrew Loza <andrejloza81@gmail.com>
2023-02-09 00:47:04 +0200

    commit

---
+++ datatipes.js
@@ -0,0 +1,15 @@
+let a = +prompt("width");
+
+let b = +prompt("height");
+
+let c = (a + b) * 2;
+
+let d = a * b;
+
+alert(c);
+
+alert(d);
+
+let x = +prompt("enter number");
+
+alert(((x + 10 - 20) * 20) / 5);
